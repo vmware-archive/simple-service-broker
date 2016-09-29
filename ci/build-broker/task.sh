90f511042456d2b4b0c73c53303613a08c815e05
JaredGordon <jgordon@pivotal.io>
2016-09-29 11:01:39 -0400

    start using semver.

--- ci/build-broker/task.sh
+++ ci/build-broker/task.sh
@@ -7,6 +7,6 @@ mvn -e install
 file=`ls target/*.jar`
 filename=$(basename "${file}")
 filename=${filename%-*}
-ver=`cat ../version/current-version`
+ver=`more ../version/number`
 
 cp ${file} ../broker-jar/${filename}-${ver}.jar
