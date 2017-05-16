8a717c94cf2ed32799572659e842db61769f7038
JaredGordon <jgordon@pivotal.io>
2017-05-16 15:14:37 -0400

    wip whole lotta tests.

--- simple-broker/src/test/java/io/pivotal/ecosystem/servicebroker/service/DefaultServiceAsync.java
+++
@@ -1,9 +0,0 @@
-package io.pivotal.ecosystem.servicebroker.service;
-
-public class DefaultServiceAsync extends DefaultServiceImpl {
-
-    @Override
-    public boolean isAsync() {
-        return true;
-    }
-}
